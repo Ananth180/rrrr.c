5515cf9a7b6c36fd201350be8a7e9c50a13bede9
Ananth180 <35765113+Ananth180@users.noreply.github.com>
2018-03-23 16:01:14 +0530

    Create ssssss.c

---
+++ ssssss.c
@@ -0,0 +1,15 @@
+#include<string.h>
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+    char s1[50],s2[50],ch=',';
+    printf("enter the string :");
+    scanf("%[^\n]s",s1);
+    printf("%c %c %c",s1[0],ch,s1[2]);
+
+}
+return 0;
+getch();
+}
